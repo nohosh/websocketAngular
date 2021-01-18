8a1a0732065b9d2a217b40add76703e2ef9361ac
Sanchit <sanchit.chaudhary@mettl.com>
2021-01-19 00:36:07 +0530

    remove try/catch

--- src/app/join-game/join-game.component.ts
+++ src/app/join-game/join-game.component.ts
@@ -54,28 +54,23 @@ export class JoinGameComponent implements OnInit {
     this.x++;
     const formValue = this.joinForm.value;
     console.log(JSON.stringify(formValue))
-    try {
-      this.http.post<ResponseDTO>(`http://localhost:8089/join`, JSON.stringify(formValue)).subscribe({
-        next: (res) => {
-          console.log(res);
-          this.snackBar.open(res.detail.toString(), res.type.toString(), {
-            duration: 2000
-          });
-          localStorage.setItem('joined', 'true');
-          localStorage.setItem('player', this.joinForm.value.name);
-          this.dialogRef.close();
-        },
-        error: (err) => {
-          console.error(err);
-          this.snackBar.open("Invalid name: There is already a player here with that name", 'Error', {
-            duration: 2000
-          });
-        }
-      });
-
-    } catch (err) {
-
-    }
+    this.http.post<ResponseDTO>(`http://localhost:8089/join`, JSON.stringify(formValue)).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.snackBar.open(res.detail.toString(), res.type.toString(), {
+          duration: 2000
+        });
+        localStorage.setItem('joined', 'true');
+        localStorage.setItem('player', this.joinForm.value.name);
+        this.dialogRef.close();
+      },
+      error: (err) => {
+        console.error(err);
+        this.snackBar.open("Invalid name: There is already a player here with that name", 'Error', {
+          duration: 2000
+        });
+      }
+    });
   }
 
 }
