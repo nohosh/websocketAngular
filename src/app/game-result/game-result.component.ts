52de8920352bafffdb10f2af2fa330a88cdcd0f1
Sanchit <sanchit.chaudhary@mettl.com>
2021-01-18 18:02:05 +0530

    winner screen

--- src/app/game-result/game-result.component.ts
+++ src/app/game-result/game-result.component.ts
@@ -1,10 +1,19 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { GamePlayer } from '../game-state/game.types';
 
 @Component({
   selector: 'app-game-result',
   templateUrl: './game-result.component.html',
-  styleUrls: ['./game-result.component.scss']
+  styleUrls: ['./game-result.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0, transform: 'scale(0.75)' }),
+        animate(150, style({ opacity: 1, transform: 'scale(1)' }))
+      ])
+    ])
+  ]
 })
 export class GameResultComponent implements OnInit {
   @Input() winner: GamePlayer
