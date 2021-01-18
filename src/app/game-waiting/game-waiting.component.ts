319d193cac98003e4b9bfc5f404b728b39d57258
Sanchit <sanchit.chaudhary@mettl.com>
2021-01-18 17:01:12 +0530

    animation on waiting

--- src/app/game-waiting/game-waiting.component.ts
+++ src/app/game-waiting/game-waiting.component.ts
@@ -1,10 +1,18 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-waiting',
   templateUrl: './game-waiting.component.html',
   styleUrls: ['./game-waiting.component.scss'],
-  animations: []
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0, transform: 'scale(0.75)' }),
+        animate(150, style({ opacity: 1, transform: 'scale(1)' }))
+      ])
+    ])
+  ]
 })
 export class GameWaitingComponent implements OnInit {
 
