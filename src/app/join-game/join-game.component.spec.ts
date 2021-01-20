fc007b1280a97860244b6df4afd457457cac8d37
Sanchit <sanchit.chaudhary@mettl.com>
2021-01-20 21:04:46 +0530

    join game test

--- src/app/join-game/join-game.component.spec.ts
+++ src/app/join-game/join-game.component.spec.ts
@@ -1,25 +1,37 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule, MatDialogRef, MatFormFieldModule, MatInputModule, MatSnackBarModule, MAT_DIALOG_DATA } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-// import { JoinGameComponent } from './join-game.component';
+import { JoinGameComponent } from './join-game.component';
 
-// describe('JoinGameComponent', () => {
-//   let component: JoinGameComponent;
-//   let fixture: ComponentFixture<JoinGameComponent>;
+describe('JoinGameComponent', () => {
+    let component: JoinGameComponent;
+    let fixture: ComponentFixture<JoinGameComponent>;
+    const mockDialogRef = {
+        close: jasmine.createSpy('close')
+    };
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ JoinGameComponent ]
-//     })
-//     .compileComponents();
-//   }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [JoinGameComponent],
+            imports: [MatFormFieldModule, MatDialogModule, FormsModule, ReactiveFormsModule, HttpClientModule, MatSnackBarModule, NoopAnimationsModule, MatInputModule],
+            providers: [{
+                provide: MatDialogRef,
+                useValue: mockDialogRef
+            }]
+        })
+            .compileComponents();
+    }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(JoinGameComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(JoinGameComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
--- src/app/leaderboard/leaderboard.component.spec.ts
+++ src/app/leaderboard/leaderboard.component.spec.ts
@@ -1,25 +1,47 @@
+// import { DebugElement } from '@angular/core';
 // import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { MatBadgeModule, MatIconModule, MatTableModule, MatTooltipModule } from '@angular/material';
+// import { By } from '@angular/platform-browser';
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 // import { LeaderboardComponent } from './leaderboard.component';
 
 // describe('LeaderboardComponent', () => {
-//   let component: LeaderboardComponent;
-//   let fixture: ComponentFixture<LeaderboardComponent>;
+//     let component: LeaderboardComponent;
+//     let fixture: ComponentFixture<LeaderboardComponent>;
+//     let de: DebugElement;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ LeaderboardComponent ]
+//     beforeEach(async(() => {
+//         TestBed.configureTestingModule({
+//             declarations: [LeaderboardComponent],
+//             imports: [
+//                 NoopAnimationsModule,
+//                 MatTableModule,
+//                 MatTooltipModule,
+//                 MatBadgeModule,
+//                 MatIconModule
+//             ]
+//         })
+//             .compileComponents();
+//     }));
+
+//     beforeEach(() => {
+//         fixture = TestBed.createComponent(LeaderboardComponent);
+//         de = fixture.debugElement;
+//         component = fixture.componentInstance;
+//         fixture.detectChanges();
+//     });
+
+//     it('should create', () => {
+//         expect(component).toBeTruthy();
+//     });
+//     it('shoud have correct rows', () => {
+//         fixture.detectChanges();
+//         const rowDebugElements = de.queryAll(By.css('tbody tr'));
+//         expect(rowDebugElements.length).toBe(0);
+//         const rowHtmlElements = de.nativeElement.querySelectorAll('tbody tr');
+//         expect(rowHtmlElements.length).toBe(5);
 //     })
-//     .compileComponents();
-//   }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LeaderboardComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
 // });
